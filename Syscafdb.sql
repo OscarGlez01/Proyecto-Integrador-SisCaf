cb9adad0ef65dd62eac022c6e6ea662a3890843e
OscarGlez01 <60475217+OscarGlez01@users.noreply.github.com>
2022-04-17 01:41:16 -0700

    Ajuste BD
    Versión 1.0 de la base de datos (revamp)

--- Syscafdb.sql
+++ Syscafdb.sql
@@ -16,28 +16,82 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `combo`
+-- Table structure for table `clientes`
 --
 
-DROP TABLE IF EXISTS `combo`;
+DROP TABLE IF EXISTS `clientes`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `combo` (
+CREATE TABLE `clientes` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(200) DEFAULT NULL,
-  `fechaInicio` datetime DEFAULT NULL,
-  `fechaFin` datetime DEFAULT NULL,
+  `nombre` varchar(150) DEFAULT NULL,
+  `correo` varchar(20) DEFAULT NULL,
+  `contrasena` varchar(150) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `combo`
+-- Dumping data for table `clientes`
 --
 
-LOCK TABLES `combo` WRITE;
-/*!40000 ALTER TABLE `combo` DISABLE KEYS */;
-/*!40000 ALTER TABLE `combo` ENABLE KEYS */;
+LOCK TABLES `clientes` WRITE;
+/*!40000 ALTER TABLE `clientes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `clientes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `combos`
+--
+
+DROP TABLE IF EXISTS `combos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `combos` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(200) DEFAULT NULL,
+  `fechaInicio` datetime DEFAULT NULL,
+  `fechaFin` datetime DEFAULT NULL,
+  `precio` double DEFAULT NULL,
+  `idMenu` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_combos_menus_idx` (`idMenu`),
+  CONSTRAINT `FK_combos_menus` FOREIGN KEY (`idMenu`) REFERENCES `menus` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `combos`
+--
+
+LOCK TABLES `combos` WRITE;
+/*!40000 ALTER TABLE `combos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `combos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `gerentes`
+--
+
+DROP TABLE IF EXISTS `gerentes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `gerentes` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(150) DEFAULT NULL,
+  `correo` varchar(20) DEFAULT NULL,
+  `contrasena` varchar(150) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `gerentes`
+--
+
+LOCK TABLES `gerentes` WRITE;
+/*!40000 ALTER TABLE `gerentes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `gerentes` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -77,6 +131,7 @@ CREATE TABLE `productos` (
   `precio` double DEFAULT NULL,
   `categoria` varchar(45) DEFAULT NULL,
   `idMenu` int DEFAULT NULL,
+  `descripcion` varchar(500) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `FK_productos_menu_idx` (`idMenu`),
   CONSTRAINT `FK_productos_menu` FOREIGN KEY (`idMenu`) REFERENCES `menus` (`id`)
@@ -91,31 +146,6 @@ LOCK TABLES `productos` WRITE;
 /*!40000 ALTER TABLE `productos` DISABLE KEYS */;
 /*!40000 ALTER TABLE `productos` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Table structure for table `usuario`
---
-
-DROP TABLE IF EXISTS `usuario`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `usuario` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(150) DEFAULT NULL,
-  `apellido` varchar(150) DEFAULT NULL,
-  `acceso` int DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `usuario`
---
-
-LOCK TABLES `usuario` WRITE;
-/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
-/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -126,4 +156,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-03-21 16:03:16
+-- Dump completed on 2022-04-17  1:34:36
